9402ceb4f96952ff52a1f2a1b29e79d3485d5ec8
Leticia <leticiagabriellourenco@gmail.com>
2020-04-22 15:05:57 -0700

    Add routing and handler to checkbox feature

--- app.py
+++ app.py
@@ -18,6 +18,20 @@ class Todo(db.Model):
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+    return redirect(url_for('index'))
+
+
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
   error = False
