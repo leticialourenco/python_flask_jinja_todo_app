4e0cb30f2660e552fdacc1ae49266d6caab9d148
Leticia <leticiagabriellourenco@gmail.com>
2020-04-23 20:59:06 -0700

    Add list views

--- app.py
+++ app.py
@@ -73,6 +73,15 @@ def create_todo():
     else:
       return redirect(url_for('index'))
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+  return render_template('index.html',
+  lists=TodoList.query.all(),
+  todos=Todo.query.filter_by(list_id=list_id).order_by('id').all(),
+  current_list=TodoList.query.get(list_id)
+)
+
+
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.order_by('id').all())
+  return redirect(url_for('get_list_todos', list_id=1))
